3b4f32e43867fa3379a125a2a4f745ebc5e5c18b
Race160 <racechase160@gmail.com>
2018-12-02 00:04:50 -0800

    Day 1 Part 1

---
+++ 01/01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)      
+        {
+            Console.WriteLine($"{Part1()}");
+
+            Console.Read();         
+        }
+
+        static int Part1()
+        {
+            string[] lines = System.IO.File.ReadAllLines("TextFile1.txt");
+
+            int total = 0;
+
+            foreach (string line in lines)
+            {                
+                total += Int32.Parse(line);
+            }
+
+            return total;
+        }
+    }
+}
